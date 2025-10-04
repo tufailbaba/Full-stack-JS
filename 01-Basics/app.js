0c619ae484ca3bd2a0b331442d92dfd8f9abe81f
Tufail <tufailbabacoc@gmail.com>
2025-10-04 21:42:42 +0530

    your message

---
+++ 01-Basics/app.js
@@ -0,0 +1,5 @@
+console.log("Hi,Tufail");
+console.log({ name:"Tufail"});
+
+//This is comment and is ignored by the computer but helps humans understand the code. Its like leaving notes inside your program
+
