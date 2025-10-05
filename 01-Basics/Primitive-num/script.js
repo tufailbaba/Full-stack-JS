6bb9a9f813c8e759961fc2493d0b6b5c88f73583
Tufail <tufailbabacoc@gmail.com>
2025-10-05 22:04:27 +0530

    AM (Comparison Operator

---
+++ 01-Basics/Primitive-num/script.js
@@ -0,0 +1,98 @@
+let num = 10000;
+
+// console.log(num);
+
+// console.log(typeof num);
+num = 12.345;
+// console.log(num);
+// console.log(typeof num);
+
+//Basic Mathematical Operations + - * / %
+/** 
+console.log(2 + 3);
+console.log(10 - 7);
+console.log(4 * 5);
+console.log(20 / 4);
+console.log(10 % 3);
+console.log(2**2);
+**/
+let counter = 0;
+counter++
+counter--
+
+// console.log(counter);
+
+//Exercises
+
+/** 
+let firstFavName = 3;
+let secondFavName = 313;
+console.log(firstFavName + secondFavName); //Addition
+console.log(firstFavName-secondFavName); //Subtraction
+console.log(firstFavName*secondFavName); //Multiplication
+console.log(firstFavName/secondFavName); //Division
+console.log(firstFavName%secondFavName); //Modulus
+console.log(firstFavName**secondFavName); //Exponentiation
+
+**/
+
+//Boolean
+
+let number = 10;
+//falsey values: 0, null, undefined, NaN, ''
+//Truthy values: Any number other than 0, ' ', 'hello', [], {}
+
+//Exercises
+/** 
+let isJsProgrammingLanguage = true;
+console.log(isJsProgrammingLanguage);
+
+let isJsHard = false;
+
+console.log(isJsHard);
+
+let favnum = 3;
+
+console.log(favnum);
+
+console.log(favnum + undefined);
+
+*/
+//Relational Operators
+
+console.log(10>10); //false
+console.log(10>=10); //true
+
+// Equality Operators 
+/** 
+console.log(10 === 10);
+console.log(10  === '10'); 
+console.log(10 !== '10');
+*/
+
+//Exercises
+
+let firstFavName = 3;
+let secondFavName = 313;
+console.log(firstFavName > secondFavName);
+console.log(secondFavName > firstFavName);
+console.log(firstFavName >= secondFavName);
+console.log(firstFavName <= secondFavName);
+console.log(firstFavName === secondFavName);
+console.log(firstFavName ==  secondFavName);
+console.log(firstFavName != secondFavName);
+console.log(firstFavName !== secondFavName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
