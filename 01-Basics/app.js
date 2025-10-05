1962a183f2c2f3426777f7c92eb5a5a6d4184e55
Tufail <tufailbabacoc@gmail.com>
2025-10-05 07:35:17 +0530

    Variables

--- 01-Basics/app.js
+++ 01-Basics/app.js
@@ -3,3 +3,9 @@ console.log({ name:"Tufail"});
 
 //This is comment and is ignored by the computer but helps humans understand the code. Its like leaving notes inside your program
 
+//Declare varaibles
+let name;
+
+//Assign values to variables
+let age = 20;
+console.log(age);
