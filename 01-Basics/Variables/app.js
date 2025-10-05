c98325063eaecca650be390c808e318003ad1277
Tufail <tufailbabacoc@gmail.com>
2025-10-05 07:51:35 +0530

    Primitive-nums

---
+++ 01-Basics/Variables/app.js
@@ -0,0 +1,25 @@
+console.log("Hi,Tufail");
+console.log({ name:"Tufail"});
+
+//This is comment and is ignored by the computer but helps humans understand the code. Its like leaving notes inside your program
+
+//Declare varaibles
+let name;
+
+//Assign values to variables
+let age = 20;
+// console.log(age);
+
+// Change the value of a variable
+
+let youtubeChannel = "Code with Tufail";
+youtubeChannel = "Code with Tufail - Updated";
+// console.log(youtubeChannel);
+//Exercise
+let myName = "Tufail";
+let whatYouWannabecomeInYourLife = "Software Engineer";
+let twitterHandle = "@tufailcodes";
+let gender = "Male";
+
+//
+
