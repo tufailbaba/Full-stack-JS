963743905282b8542d50d2668dfefd5287bd913c
Tufail <tufailbabacoc@gmail.com>
2025-10-09 12:02:13 +0530

    Functions

--- Objects/app.js
+++ Objects/app.js
@@ -1,8 +1,23 @@
 //Objects
 
-  const person = {
-    firstName : "Tufail",
-    lastName : "Baba"
-  }
-  console.log(person);
-  
+const person = {
+  firstName: "Tufail",
+  lastName: "Baba",
+  age: 20,
+  location: ["planet", "Earth"],
+  isProgrammer: true,
+};
+console.log(person);
+//Acessing Items from our Object
+console.log(person.age);
+console.log(person["firstName"]);
+// Exercises
+
+const car = {
+  type: "Suzuki",
+  model: 2025,
+  color: "Grey",
+};
+car.type ="Toyota"
+car.wheels= 4
+console.log(car);
