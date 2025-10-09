04994bd851b038dec59fc09a1ca326eee32063a9
Tufail <tufailbabacoc@gmail.com>
2025-10-09 12:05:22 +0530

    ret,stor,log-- function

---
+++ Functions/app.js
@@ -0,0 +1,20 @@
+//Functions 
+function greet (){
+    console.log("Hello from function");
+    
+}
+// Call , run , execute
+greet();
+
+function add (x ,y){
+    return x+y
+} 
+// console.log(add(10,7));
+
+const rest  = add(10,20);
+
+const rest1 = add (10,2);
+console.log(rest);
+console.log(rest1);
+
+
