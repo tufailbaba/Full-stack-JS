7e79f2695422e6652509ddd2bfbf67504c720b14
Tufail <tufailbabacoc@gmail.com>
2025-10-09 08:34:04 +0530

    Array Methods

---
+++ Arrays/app.js
@@ -0,0 +1,34 @@
+//Arrrays -> 0 index based
+
+//Empty array
+
+const myList = [1,2,3,4]
+// console.log(myList);
+
+const stringArr = ["eat",'Sleep',"code","Repeat"]
+// console.log(stringArr);
+
+//Accessing element from array
+
+console.log(stringArr[1]);
+//Nested Array || Two Dimension Array
+
+const nestArr = ['One','two',['three','four']]
+// console.log(nestArr[2]);
+
+//Exercices
+
+const favSinger = ['Faheem', 'karan', 'Lana del']
+console.log(favSinger[0]);
+const favNumber = [3,7,11];
+console.log(favNumber);
+
+const mixedArr = ['String', ['Otherarray'],123,true]
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+
+
+
+
---
+++ Arrays/methods.js
@@ -0,0 +1,28 @@
+const fruits = [ 
+    'Apples',
+    'Banana',
+    'Grapes'
+]
+fruits.push('Mango')
+fruits.includes('Apples')
+fruits.pop()
+fruits.shift()
+fruits.unshift("Kuku")
+const newFruits = [
+    'Tun tun',
+    'Boka'
+]
+const totalFruits = fruits.concat(newFruits);
+
+console.log(totalFruits.slice(0));
+
+
+
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits.includes("Kuku"));
+
+
+
+
+
