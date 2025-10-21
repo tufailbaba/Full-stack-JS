53000c324510131a12fc5be001a567d9bef73931
Tufail <tufailbabacoc@gmail.com>
2025-10-21 08:44:52 +0530

    Template string

---
+++ Template_String/app.js
@@ -0,0 +1,23 @@
+const firstName = "Tufail";
+const lastname = "WebDev"
+
+// console.log(firstName + "" +lastname );
+
+
+// console.log(` My name is: ${firstName} ${lastname}`); template string
+
+function info (){
+    return `My name is Tufail WebDev & I'm 20 year old.`;
+}
+console.log(`${info()}`);
+
+// Challenge
+const text = `The quick
+brown fox
+jumps over 
+the lazy dog`;
+const firstname = "Tufi";
+const lastName = "Web dev"
+console.log(` ${firstname} ${lastName}`);
+
+
