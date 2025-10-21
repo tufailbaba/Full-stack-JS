2796c5798cdaacec71d1f80a778c77b2ce668618
Tufail <tufailbabacoc@gmail.com>
2025-10-21 09:36:24 +0530

    Enchanced object literals

---
+++ Arrow_function/app.js
@@ -0,0 +1,24 @@
+function greet (username){
+    return`Hello ${username}`;
+}
+// console.log(greet("Tufail"));
+
+greeT = Username =>`Hello ${Username}`;
+
+console.log(greeT('tufail'));
+
+// function dogna (number){
+//     return number*2
+// }
+// console.log(dogna(4));
+
+// Double = Number =>` ${Number*2}`
+// console.log(Double(5));
+
+const Double = n => n*2;
+console.log(Double(10));
+
+// challenge
+
+
+
