4a730bbc6d510c4e1f60b48c6d73180e88bed050
Tufail <tufailbabacoc@gmail.com>
2025-10-24 08:29:32 +0530

    object Destructuring

---
+++ Object_Destructuring/script.js
@@ -0,0 +1,20 @@
+const student  = {Name:"Tufail",
+    position:"first",
+    rollNo:103
+}
+const{Name}=student;
+console.log(Name);
+// challenge
+
+const person ={
+    name:"Tufail baba",
+    age:20,
+    gender:"Male",
+    country:"Kashmir"
+};
+
+const{name, age, country}=person;
+console.log(name);
+console.log(age);
+console.log(country);
+
