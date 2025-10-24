ce62f97d5416d4e52014b0db207e6ec91353777d
Tufail <tufailbabacoc@gmail.com>
2025-10-24 10:00:41 +0530

    For-of loop

--- For loops/script.js
+++ For loops/script.js
@@ -1,3 +1,4 @@
+// for-in  loop is used to iterate over the properties of an object.
 let person = {
     name: "tufail",
     age: 20,
@@ -20,5 +21,13 @@ for(let index in list){
 const object = {a:1, b:2, c:3};
 for (const properties in object ){
     console.log(`${properties}:${object[properties]}`);
+    
+}
+
+// for-of loop is used to iterate over iterable objects like arrays, strings, maps, sets, etc.
+
+let peoples = ["Tufi", "alex",'baba']
+for (let people of peoples){
+    console.log(peoples);
     
 }
