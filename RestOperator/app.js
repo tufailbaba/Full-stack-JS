3058860c5f0c1d5865c2ebc7c5b20d1a244cf28c
Tufail <tufailbabacoc@gmail.com>
2025-10-24 08:16:54 +0530

    Assigning values to the elements

--- RestOperator/app.js
+++ RestOperator/app.js
@@ -22,3 +22,14 @@ console.log(12,1,3,2,2,24);
 const foo = ["one", "Two", "Three"];
 const one = foo[0] 
 console.log(one);
+
+//Assigining the rest of the array elements to a variable
+const [a, ...b]=['one','two', 'three']
+console.log(b);
+//challenge
+
+const colors = ['red','green', 'blue', 'yellow','orange'];
+
+const [color1,color2,color3]= colors;
+console.log(color1,color2,color3);
+
