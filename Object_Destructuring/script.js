5075c61e1b6ee37c342d66d2394e75879ff5ea12
Tufail <tufailbabacoc@gmail.com>
2025-10-24 08:31:54 +0530

    Renaming Variables

--- Object_Destructuring/script.js
+++ Object_Destructuring/script.js
@@ -18,3 +18,11 @@ console.log(name);
 console.log(age);
 console.log(country);
 
+// renaming Variables
+
+const num = {x:100, y:200}
+const {x:new1, y:new2}=num
+
+console.log(new1);
+console.log(new2);
+
