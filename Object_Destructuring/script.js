e0cf09ee70dc7b575e2f88bce416a628bc8d553a
Tufail <tufailbabacoc@gmail.com>
2025-10-24 08:42:36 +0530

    Object Destructuring and rest Operator

--- Object_Destructuring/script.js
+++ Object_Destructuring/script.js
@@ -26,3 +26,10 @@ const {x:new1, y:new2}=num
 console.log(new1);
 console.log(new2);
 
+//object destructuring and rest operator
+
+let {a,b, ...rest}={a:100,b:200,c:300,d:400,e:500}
+// console.log(a);
+// console.log(b);
+console.log(rest);
+
