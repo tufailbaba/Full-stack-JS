af7b160cb847068ed25991bd9e800db287e88215
Tufail <tufailbabacoc@gmail.com>
2025-10-24 10:10:25 +0530

    For-each loop

--- For loops/script.js
+++ For loops/script.js
@@ -31,3 +31,27 @@ for (let people of peoples){
     console.log(peoples);
     
 }
+const text ='Hello'
+for (const char of text) {
+    console.log(text);
+    
+    
+}
+
+//challenge
+
+const array1 = ["a", "b", "c"];
+for (const elements of array1 ) {
+    console.log(array1);
+    
+    
+}
+
+// foreach loop is a method that executes a provided function once for each array element.
+ 
+const colors = ["teal", "blue",'red']
+colors.forEach(element => {
+    console.log(colors);
+    
+});
+
