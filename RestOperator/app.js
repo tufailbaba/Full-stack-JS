b2114ee562dd1437e8b9d35f3ee04ff1e4ac7efa
Tufail <tufailbabacoc@gmail.com>
2025-10-24 08:11:33 +0530

    Destructuring

---
+++ RestOperator/app.js
@@ -0,0 +1,24 @@
+function person(firstName, LastName, ...hobbies) {
+  console.log("First Name :", firstName);
+  console.log("Last Name :", LastName);
+  console.log("Hobbies:", hobbies);
+  
+}
+
+person("Tufail", "WebDev","Programming","Cricket","Riding")
+
+
+// Challenge
+
+function unlimited (...unlimited){
+    console.log("Unlimited amount" , unlimited);
+    
+}
+console.log(12,1,3,2,2,24);
+
+
+//Destructuring
+
+const foo = ["one", "Two", "Three"];
+const one = foo[0] 
+console.log(one);
